357df3ebf80189cf1b0d08487d06715f399b799b
José Romero <jose.romero@volvocars.com>
2023-03-02 14:49:01 +0100

    Fixed Fenwick tree implementation

--- tests/unit/data_structures/fenwick_tree.cc
+++ tests/unit/data_structures/fenwick_tree.cc
@@ -1,3 +1,5 @@
+#pragma once
+
 #include "code/data_structures/fenwick_tree.hpp"
 
 #include <tuple>
@@ -6,8 +8,8 @@
 #include "catch2/catch.hpp"
 
 template <typename T>
-fenwick<T> build_fenwick_tree(std::vector<T> v) {
-    int N = (int)v.size();
+fenwick<T> build_fenwick_tree(const std::vector<T> v) {
+    const int N = (int)v.size();
     fenwick<T> f(N);
     for(int i = 0; i < N; ++i)
         f.update(i + 1, v[i]);
@@ -18,16 +20,16 @@ TEST_CASE("SumFenwickTree", "[fenwick_tree]") {
     // Calculate the sum of a all elements in a range.
     // Range is [a, b) and 0-index.
 
-    std::vector<int> a = {1, 10, 100, 1000, 10000};
-    std::vector<std::pair<int, int>> queries = {{2, 3}, {0, 3}, {2, 5}, {3, 4}, {0, 5}};
-    std::vector<int> expected = {100, 111, 11100, 1000, 11111};
+    const std::vector<int> a = {1, 10, 100, 1000, 10000};
+    const std::vector<std::pair<int, int>> queries = {{2, 3}, {0, 3}, {2, 5}, {3, 4}, {0, 5}};
+    const std::vector<int> expected = {100, 111, 11100, 1000, 11111};
 
     fenwick<int> f = build_fenwick_tree(a);
-    int Q = (int)queries.size();
+    const int Q = (int)queries.size();
     for(int i = 0; i < Q; ++i) {
-        int left = queries[i].first;
-        int right = queries[i].second;
-        int result = f.query(right) - f.query(left);
+        const int left = queries[i].first;
+        const int right = queries[i].second;
+        const int result = f.query(right) - f.query(left);
         CAPTURE(i, left, right, result, expected[i]);
         REQUIRE(result == expected[i]);
     }
@@ -49,31 +51,32 @@ TEST_CASE("XorFenwickTree", "[fenwick_tree]") {
     };
 
     SECTION("3 queries: range -> update -> range") {
-        std::vector<_xor> a = {_xor{2},
-                               _xor{1},
-                               _xor{1},
-                               _xor{3},
-                               _xor{2},
-                               _xor{3},
-                               _xor{4},
-                               _xor{5},
-                               _xor{6},
-                               _xor{7},
-                               _xor{8},
-                               _xor{9}};
-        std::vector<std::tuple<int, int, int>> queries = {{1, 3, 9}, {2, 5, 6}, {1, 3, 9}};
-        std::vector<int> expected = {5, 3};
+        const std::vector<_xor> a = {_xor{2},
+                                     _xor{1},
+                                     _xor{1},
+                                     _xor{3},
+                                     _xor{2},
+                                     _xor{3},
+                                     _xor{4},
+                                     _xor{5},
+                                     _xor{6},
+                                     _xor{7},
+                                     _xor{8},
+                                     _xor{9}};
+        const std::vector<std::tuple<int, int, int>> queries = {{1, 3, 9}, {2, 5, 6}, {1, 3, 9}};
+        const std::vector<int> expected = {5, 3};
 
         fenwick<_xor> f = build_fenwick_tree(a);
-        int Q = (int)queries.size(), cnt = 0;
-        for(int i = 0; i < Q; ++i) {
-            int t = std::get<0>(queries[i]);
-            int left = std::get<1>(queries[i]);
-            int right = std::get<2>(queries[i]);
+        const int Q = (int)queries.size();
+        for(int i = 0, cnt = 0; i < Q; ++i) {
+            const int t = std::get<0>(queries[i]);
+            const int left = std::get<1>(queries[i]);
+            const int right = std::get<2>(queries[i]);
 
-            CAPTURE(i, cnt, t, left, right, result, expected[cnt]);
+            CAPTURE(i, cnt, t, left, right, expected[cnt]);
             if(t == 1) {
-                int result = f.query(right).val ^ f.query(left).val;
+                const int result = f.query(right).val ^ f.query(left).val;
+                CAPTURE(result);
                 REQUIRE(result == expected[cnt]);
                 ++cnt;
             }
@@ -84,34 +87,36 @@ TEST_CASE("XorFenwickTree", "[fenwick_tree]") {
     }
 
     SECTION("5 queries: range -> update -> update -> update -> range") {
-        std::vector<_xor> a = {_xor{2},
-                               _xor{1},
-                               _xor{1},
-                               _xor{3},
-                               _xor{2},
-                               _xor{3},
-                               _xor{4},
-                               _xor{5},
-                               _xor{6},
-                               _xor{7},
-                               _xor{8},
-                               _xor{9}};
-        std::vector<std::tuple<int, int, int>> queries = {{1, 0, 10},
-                                                          {2, 4, 6},
-                                                          {2, 6, 5},
-                                                          {2, 9, 1},
-                                                          {1, 0, 10}};
-        std::vector<int> expected = {0, 2};
+        const std::vector<_xor> a = {_xor{2},
+                                     _xor{1},
+                                     _xor{1},
+                                     _xor{3},
+                                     _xor{2},
+                                     _xor{3},
+                                     _xor{4},
+                                     _xor{5},
+                                     _xor{6},
+                                     _xor{7},
+                                     _xor{8},
+                                     _xor{9}};
+        const std::vector<std::tuple<int, int, int>> queries = {{1, 0, 10},
+                                                                {2, 4, 6},
+                                                                {2, 6, 5},
+                                                                {2, 9, 1},
+                                                                {1, 0, 10}};
+        const std::vector<int> expected = {0, 2};
 
         fenwick<_xor> f = build_fenwick_tree(a);
-        int Q = (int)queries.size(), cnt = 0;
-        for(int i = 0; i < Q; ++i) {
-            int t = std::get<0>(queries[i]), left = std::get<1>(queries[i]),
-                right = std::get<2>(queries[i]);
+        const int Q = (int)queries.size();
+        for(int i = 0, cnt = 0; i < Q; ++i) {
+            const int t = std::get<0>(queries[i]);
+            const int left = std::get<1>(queries[i]);
+            const int right = std::get<2>(queries[i]);
 
-            CAPTURE(i, cnt, t, left, right, result, expected[cnt]);
+            CAPTURE(i, cnt, t, left, right, expected[cnt]);
             if(t == 1) {
-                int result = f.query(right).val ^ f.query(left).val;
+                const int result = f.query(right).val ^ f.query(left).val;
+                CAPTURE(result);
                 REQUIRE(result == expected[cnt]);
                 ++cnt;
             }
