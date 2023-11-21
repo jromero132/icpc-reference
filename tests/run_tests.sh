256e5fcf047f3c0c0640956c24546f776f751a35
Jose Ariel Romero <josea132.romero@gmail.com>
2023-11-21 16:48:21 +0100

    Fix colors in run_tests.sh script

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -4,12 +4,12 @@
 STD=c++11
 
 # Colors
-COLOR_RED=`tput setaf 1`
-COLOR_GREEN=`tput setaf 2`
-COLOR_YELLOW=`tput setaf 3`
-COLOR_CYAN=`tput setaf 6`
-COLOR_GRAY=`tput setaf 008`
-NO_COLOR=`tput sgr0`
+COLOR_RED="\033[31m"
+COLOR_GREEN="\033[32m"
+COLOR_YELLOW="\033[33m"
+COLOR_CYAN="\033[36m"
+COLOR_GRAY="\033[38;2;100;100;100m"
+NO_COLOR="\033[0m"
 
 EXIT_CODE=0
 
@@ -27,7 +27,7 @@ failed_tests=()
 
 for cppfile in $(find * -type f -name 'test_*.cpp');
 do
-    echo "${COLOR_CYAN}›${NO_COLOR} Running... [${COLOR_CYAN}$cppfile${NO_COLOR}]"
+    echo -e "${COLOR_CYAN}›${NO_COLOR} Running... [${COLOR_CYAN}$cppfile${NO_COLOR}]"
     start_time=`date +%s%N`
 
     $CXX -std=$STD -I . -I ../ -fsanitize=undefined -fno-sanitize-recover $cppfile -o $cppfile.out
@@ -40,32 +40,32 @@ do
         if [ $test_success -eq 0 ];
         then
             success=$((success + 1))
-            echo -n "  ${COLOR_GREEN}› PASSED${NO_COLOR}"
+            echo -en "  ${COLOR_GREEN}› PASSED${NO_COLOR}"
         else
-            echo -n "  ${COLOR_RED}› FAILED${NO_COLOR}"
+            echo -en "  ${COLOR_RED}› FAILED${NO_COLOR}"
             failed_tests+=($cppfile)
         fi
-        echo " ${COLOR_GRAY}($(((end_time - $start_time)/1000000))ms)${NO_COLOR}"
+        echo -e " ${COLOR_GRAY}($(((end_time - $start_time)/1000000))ms)${NO_COLOR}"
 
         rm $cppfile.out
     else
-        echo "${COLOR_YELLOW}"Compilation error on $cppfile!"${NO_COLOR}"
+        echo -e "  ${COLOR_YELLOW}"Compilation error on $cppfile!"${NO_COLOR}"
         failed_tests+=($cppfile)
     fi
 done
 
 tests_end_time=`date +%s%N`
-time_taken=$(echo "$tests_start_time $tests_end_time" | awk '{printf "%.2f\n", ($2-$1)/1000000000.0}')
+time_taken=$(echo -e "$tests_start_time $tests_end_time" | awk '{printf "%.2f\n", ($2-$1)/1000000000.0}')
 
 if [ ${#failed_tests[@]} -eq 0 ];
 then
     echo -e "\n${COLOR_GREEN}$success PASSED${NO_COLOR} in ${time_taken}s"
 else
     echo -e "${COLOR_RED}\n${#failed_tests[@]} FAILED, ${COLOR_GREEN}$success PASSED${NO_COLOR} in ${time_taken}s\n"
-    echo "Failed tests:"
+    echo -e "Failed tests:"
     for cppfile in "${failed_tests[@]}"
     do
-        echo "  › $cppfile"
+        echo -e "  › $cppfile"
     done
     EXIT_CODE=1
 fi
